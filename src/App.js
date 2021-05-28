271ba09d5205fb725a8bf484e1be6b4031e147fa
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2021-05-27 21:08:33 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import makeStyles from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { FormGroup, TextField, withTheme } from '@material-ui/core';
 import logo from './logo.svg';
 import './App.css';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+
+
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
+      <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              JoDo List
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
       <header className="App-header">
+        <FormGroup>
+          <TextField id="new-todo" className="newTodo" label="New Todo" variant="filled"></TextField>
+        </FormGroup>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p><Button onClick={() => { alert('clicked') }} variant="contained" color="primary">Yo Joe!</Button></p>
         <a
           className="App-link"
           href="https://reactjs.org"
